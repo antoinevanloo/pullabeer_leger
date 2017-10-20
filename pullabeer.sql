68564c6aaa0563d9284df37a1a35996289f23932
antoinevanloo <vanlooantoine@gmail.com>
2017-10-20 15:11:28 +0200

    modification_mdp_utilisateur_antoine
    utilisateur antoine = administrateur site web

--- pullabeer.sql
+++ pullabeer.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 23 Février 2017 à 20:50
--- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Généré le :  Ven 20 Octobre 2017 à 15:10
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `pullabeer`
@@ -26,11 +26,13 @@ SET time_zone = "+00:00";
 -- Structure de la table `artiste`
 --
 
-CREATE TABLE `artiste` (
-  `arti_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `artiste` (
+  `arti_id` int(11) NOT NULL AUTO_INCREMENT,
   `arti_nom` varchar(80) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `arti_val` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `arti_val` int(11) NOT NULL,
+  PRIMARY KEY (`arti_id`),
+  KEY `fk_arti_val` (`arti_val`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
 --
 -- Contenu de la table `artiste`
@@ -50,13 +52,14 @@ INSERT INTO `artiste` (`arti_id`, `arti_nom`, `arti_val`) VALUES
 -- Structure de la table `bar`
 --
 
-CREATE TABLE `bar` (
+CREATE TABLE IF NOT EXISTS `bar` (
   `bar_id` int(11) NOT NULL,
   `bar_nom` varchar(20) NOT NULL,
   `bar_adrl1` varchar(250) NOT NULL,
   `bar_adrl2` varchar(250) DEFAULT NULL,
   `bar_cp` char(5) NOT NULL,
-  `bar_ville` varchar(150) NOT NULL
+  `bar_ville` varchar(150) NOT NULL,
+  PRIMARY KEY (`bar_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -75,9 +78,10 @@ INSERT INTO `bar` (`bar_id`, `bar_nom`, `bar_adrl1`, `bar_adrl2`, `bar_cp`, `bar
 -- Structure de la table `civilite`
 --
 
-CREATE TABLE `civilite` (
+CREATE TABLE IF NOT EXISTS `civilite` (
   `civ_id` int(11) NOT NULL,
-  `civ_lib` varchar(20) NOT NULL
+  `civ_lib` varchar(20) NOT NULL,
+  PRIMARY KEY (`civ_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -95,10 +99,12 @@ INSERT INTO `civilite` (`civ_id`, `civ_lib`) VALUES
 -- Structure de la table `compose`
 --
 
-CREATE TABLE `compose` (
+CREATE TABLE IF NOT EXISTS `compose` (
   `comp_ll` int(11) NOT NULL,
   `comp_mus` int(11) NOT NULL,
-  `comp_nbmus` int(11) DEFAULT NULL
+  `comp_nbmus` int(11) DEFAULT NULL,
+  PRIMARY KEY (`comp_ll`,`comp_mus`),
+  KEY `fk_comp_mus` (`comp_mus`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -121,14 +127,16 @@ INSERT INTO `compose` (`comp_ll`, `comp_mus`, `comp_nbmus`) VALUES
 -- Structure de la table `contact`
 --
 
-CREATE TABLE `contact` (
-  `contact_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `contact` (
+  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
   `contact_bar` int(11) DEFAULT NULL,
   `contact_nom` varchar(150) NOT NULL,
   `contact_mail` varchar(250) NOT NULL,
   `contact_objet` varchar(150) DEFAULT NULL,
-  `contact_message` varchar(1024) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `contact_message` varchar(1024) DEFAULT NULL,
+  PRIMARY KEY (`contact_id`),
+  KEY `fk_cont_bar` (`contact_bar`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Contenu de la table `contact`
@@ -146,8 +154,9 @@ INSERT INTO `contact` (`contact_id`, `contact_bar`, `contact_nom`, `contact_mail
 -- Structure de la table `dates`
 --
 
-CREATE TABLE `dates` (
-  `dates` date NOT NULL
+CREATE TABLE IF NOT EXISTS `dates` (
+  `dates` date NOT NULL,
+  PRIMARY KEY (`dates`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -156,16 +165,17 @@ CREATE TABLE `dates` (
 -- Structure de la table `evenement`
 --
 
-CREATE TABLE `evenement` (
-  `even_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `evenement` (
+  `even_id` int(11) NOT NULL AUTO_INCREMENT,
   `even_nom` varchar(150) DEFAULT NULL,
   `even_adrl1` varchar(250) DEFAULT NULL,
   `even_adrl2` varchar(250) DEFAULT NULL,
   `even_cp` char(5) DEFAULT NULL,
   `even_ville` varchar(150) DEFAULT NULL,
   `even_chemin` varchar(250) NOT NULL,
-  `even_comm` varchar(400) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `even_comm` varchar(400) NOT NULL,
+  PRIMARY KEY (`even_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `evenement`
@@ -182,9 +192,10 @@ INSERT INTO `evenement` (`even_id`, `even_nom`, `even_adrl1`, `even_adrl2`, `eve
 -- Structure de la table `genre`
 --
 
-CREATE TABLE `genre` (
+CREATE TABLE IF NOT EXISTS `genre` (
   `genre_id` int(11) NOT NULL,
-  `genre_lib` varchar(50) NOT NULL
+  `genre_lib` varchar(50) NOT NULL,
+  PRIMARY KEY (`genre_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -209,9 +220,11 @@ INSERT INTO `genre` (`genre_id`, `genre_lib`) VALUES
 -- Structure de la table `informe`
 --
 
-CREATE TABLE `informe` (
+CREATE TABLE IF NOT EXISTS `informe` (
   `inf_eve` int(11) NOT NULL,
-  `inf_uti` int(11) NOT NULL
+  `inf_uti` int(11) NOT NULL,
+  PRIMARY KEY (`inf_eve`,`inf_uti`),
+  KEY `fk_inf_uti` (`inf_uti`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -220,11 +233,13 @@ CREATE TABLE `informe` (
 -- Structure de la table `listelecture`
 --
 
-CREATE TABLE `listelecture` (
-  `ll_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `listelecture` (
+  `ll_id` int(11) NOT NULL AUTO_INCREMENT,
   `ll_titre` varchar(50) NOT NULL,
-  `ll_uti` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `ll_uti` int(11) NOT NULL,
+  PRIMARY KEY (`ll_id`),
+  KEY `fk_ll_uti` (`ll_uti`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=67 ;
 
 --
 -- Contenu de la table `listelecture`
@@ -236,7 +251,8 @@ INSERT INTO `listelecture` (`ll_id`, `ll_titre`, `ll_uti`) VALUES
 (60, 'Ma Playlist', 16),
 (61, 'Mes meilleurs morceaux', 17),
 (62, 'Rock', 18),
-(65, 'ma liste de lecture ', 15);
+(65, 'ma liste de lecture ', 15),
+(66, 'Ma Playliste1', 11);
 
 -- --------------------------------------------------------
 
@@ -244,15 +260,19 @@ INSERT INTO `listelecture` (`ll_id`, `ll_titre`, `ll_uti`) VALUES
 -- Structure de la table `musique`
 --
 
-CREATE TABLE `musique` (
-  `mus_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `musique` (
+  `mus_id` int(11) NOT NULL AUTO_INCREMENT,
   `mus_titre` varchar(70) NOT NULL,
   `mus_chemin` varchar(250) DEFAULT NULL,
   `mus_arti` int(11) NOT NULL,
   `mus_genre` int(11) NOT NULL,
   `mus_lien` varchar(250) DEFAULT NULL,
-  `mus_val` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `mus_val` int(11) NOT NULL,
+  PRIMARY KEY (`mus_id`),
+  KEY `fk_mus_arti` (`mus_arti`),
+  KEY `fk_mus_genre` (`mus_genre`),
+  KEY `fk_mus_val` (`mus_val`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
 
 --
 -- Contenu de la table `musique`
@@ -270,9 +290,10 @@ INSERT INTO `musique` (`mus_id`, `mus_titre`, `mus_chemin`, `mus_arti`, `mus_gen
 -- Structure de la table `permission`
 --
 
-CREATE TABLE `permission` (
+CREATE TABLE IF NOT EXISTS `permission` (
   `perm_id` int(11) NOT NULL,
-  `perm_lib` varchar(20) NOT NULL
+  `perm_lib` varchar(20) NOT NULL,
+  PRIMARY KEY (`perm_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -290,8 +311,8 @@ INSERT INTO `permission` (`perm_id`, `perm_lib`) VALUES
 -- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
-  `uti_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `uti_id` int(11) NOT NULL AUTO_INCREMENT,
   `uti_nom` varchar(70) DEFAULT NULL,
   `uti_prenom` varchar(70) DEFAULT NULL,
   `uti_mail` varchar(150) DEFAULT NULL,
@@ -303,8 +324,11 @@ CREATE TABLE `utilisateur` (
   `uti_ville` varchar(50) NOT NULL,
   `uti_barpref` varchar(50) DEFAULT NULL,
   `uti_perm` int(11) NOT NULL,
-  `uti_civ` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `uti_civ` int(11) NOT NULL,
+  PRIMARY KEY (`uti_id`),
+  KEY `fk_uti_perm` (`uti_perm`),
+  KEY `fk_uti_civ` (`uti_civ`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 --
 -- Contenu de la table `utilisateur`
@@ -312,7 +336,7 @@ CREATE TABLE `utilisateur` (
 
 INSERT INTO `utilisateur` (`uti_id`, `uti_nom`, `uti_prenom`, `uti_mail`, `uti_mdp`, `uti_naiss`, `uti_adrl1`, `uti_adrl2`, `uti_cp`, `uti_ville`, `uti_barpref`, `uti_perm`, `uti_civ`) VALUES
 (2, 'GalanÃ©', 'ValÃ©rie', 'valerie.galane@free.fr', 'azerty', '1964-09-20', '30 rue franÃ§ois couperin', '', '78370', 'Plaisir', '2', 2, 2),
-(9, 'Van Loo', 'Antoine', 'antoine@gmail.com', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', '1996-12-27', '30 rue franÃ§ois couperin', '', '00000', 'Plaisir', '1', 3, 1),
+(9, 'Van Loo', 'Antoine', 'antoine@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', '1996-12-27', '30 rue franÃ§ois couperin', '', '00000', 'Plaisir', '1', 3, 1),
 (11, 'clÃ©ment', 'loriaux', 'clement.loriaux@epsi.fr', '9cf95dacd226dcf43da376cdb6cbba7035218921', '1994-03-09', '3 rue du sdf', '', '00000', 'SDFLAND', '1', 2, 1),
 (14, 'Arnould', 'Ninon', 'ninonceleste@gmail.com', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8', '1998-03-02', '40 rue de Choisit', '', '33910', 'saint-ciers-dabzac', '1', 2, 2),
 (15, 'Van Loo', 'Camille', 'camille@gmail.com', '9cf95dacd226dcf43da376cdb6cbba7035218921', '2003-12-29', '30 rue franÃ§ois couperin', '', '78370', 'Plaisir', '1', 3, 1),
@@ -326,9 +350,10 @@ INSERT INTO `utilisateur` (`uti_id`, `uti_nom`, `uti_prenom`, `uti_mail`, `uti_m
 -- Structure de la table `validation`
 --
 
-CREATE TABLE `validation` (
+CREATE TABLE IF NOT EXISTS `validation` (
   `val_id` int(11) NOT NULL,
-  `val_lib` varchar(50) DEFAULT NULL
+  `val_lib` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`val_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -340,138 +365,6 @@ INSERT INTO `validation` (`val_id`, `val_lib`) VALUES
 (2, 'En attente'),
 (3, 'Refuser');
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `artiste`
---
-ALTER TABLE `artiste`
-  ADD PRIMARY KEY (`arti_id`),
-  ADD KEY `fk_arti_val` (`arti_val`);
-
---
--- Index pour la table `bar`
---
-ALTER TABLE `bar`
-  ADD PRIMARY KEY (`bar_id`);
-
---
--- Index pour la table `civilite`
---
-ALTER TABLE `civilite`
-  ADD PRIMARY KEY (`civ_id`);
-
---
--- Index pour la table `compose`
---
-ALTER TABLE `compose`
-  ADD PRIMARY KEY (`comp_ll`,`comp_mus`),
-  ADD KEY `fk_comp_mus` (`comp_mus`);
-
---
--- Index pour la table `contact`
---
-ALTER TABLE `contact`
-  ADD PRIMARY KEY (`contact_id`),
-  ADD KEY `fk_cont_bar` (`contact_bar`);
-
---
--- Index pour la table `dates`
---
-ALTER TABLE `dates`
-  ADD PRIMARY KEY (`dates`);
-
---
--- Index pour la table `evenement`
---
-ALTER TABLE `evenement`
-  ADD PRIMARY KEY (`even_id`);
-
---
--- Index pour la table `genre`
---
-ALTER TABLE `genre`
-  ADD PRIMARY KEY (`genre_id`);
-
---
--- Index pour la table `informe`
---
-ALTER TABLE `informe`
-  ADD PRIMARY KEY (`inf_eve`,`inf_uti`),
-  ADD KEY `fk_inf_uti` (`inf_uti`);
-
---
--- Index pour la table `listelecture`
---
-ALTER TABLE `listelecture`
-  ADD PRIMARY KEY (`ll_id`),
-  ADD KEY `fk_ll_uti` (`ll_uti`);
-
---
--- Index pour la table `musique`
---
-ALTER TABLE `musique`
-  ADD PRIMARY KEY (`mus_id`),
-  ADD KEY `fk_mus_arti` (`mus_arti`),
-  ADD KEY `fk_mus_genre` (`mus_genre`),
-  ADD KEY `fk_mus_val` (`mus_val`);
-
---
--- Index pour la table `permission`
---
-ALTER TABLE `permission`
-  ADD PRIMARY KEY (`perm_id`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`uti_id`),
-  ADD KEY `fk_uti_perm` (`uti_perm`),
-  ADD KEY `fk_uti_civ` (`uti_civ`);
-
---
--- Index pour la table `validation`
---
-ALTER TABLE `validation`
-  ADD PRIMARY KEY (`val_id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `artiste`
---
-ALTER TABLE `artiste`
-  MODIFY `arti_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
---
--- AUTO_INCREMENT pour la table `contact`
---
-ALTER TABLE `contact`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT pour la table `evenement`
---
-ALTER TABLE `evenement`
-  MODIFY `even_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT pour la table `listelecture`
---
-ALTER TABLE `listelecture`
-  MODIFY `ll_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
---
--- AUTO_INCREMENT pour la table `musique`
---
-ALTER TABLE `musique`
-  MODIFY `mus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
---
--- AUTO_INCREMENT pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `uti_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- Contraintes pour les tables exportées
 --
